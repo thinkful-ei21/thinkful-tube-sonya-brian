f9cfd990edb5fcaaf254e0811dd68951b9df600d
Sonya Slegers <sonjay08@gmail.com>
2018-05-15 11:17:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
+
+$.getJSON("https://www.googleapis.com/youtube/v3/search", (response) => {
+  console.log(response);
+})
+
+function(response) {
+
+}
